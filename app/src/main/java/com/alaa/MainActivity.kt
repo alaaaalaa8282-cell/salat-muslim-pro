b3a31e79b6f332f6696de06c63e5c7c9f563d283
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-23 11:08:40 +0300

    Update MainActivity.kt

--- app/src/main/java/com/alaa/MainActivity.kt
+++ app/src/main/java/com/alaa/MainActivity.kt
@@ -7,13 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.alaa.navigation.AppNavHost
 import com.alaa.ui.theme.AlaAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
