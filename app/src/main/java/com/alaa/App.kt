ec98b083ffabf5e1277a150255afbe7ef1488897
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-23 16:26:03 +0300

    Update App.kt

--- app/src/main/java/com/alaa/App.kt
+++ app/src/main/java/com/alaa/App.kt
@@ -6,18 +6,14 @@ import com.alaa.data.repository.PrayerRepository
 import com.alaa.data.repository.WeatherRepository
 import com.alaa.presentation.dhikr.DhikrViewModel
 import com.alaa.presentation.home.HomeViewModel
-import com.google.gson.GsonBuilder
-import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
-import org.koin.androidx.viewmodel.dsl.viewModelOf
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
 import org.koin.core.logger.Level
-import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 
 class App : Application() {
     override fun onCreate() {
@@ -27,32 +23,20 @@ class App : Application() {
             androidContext(this@App)
             modules(
                 module {
-                    // Prefs
                     single { PrefsManager(androidContext()) }
 
-                    // Network
-                    single { GsonBuilder().create() }
-                    single {
-                        OkHttpClient.Builder()
-                            .connectTimeout(30, TimeUnit.SECONDS)
-                            .readTimeout(30, TimeUnit.SECONDS)
-                            .build()
-                    }
                     single {
                         Retrofit.Builder()
                             .baseUrl("https://api.open-meteo.com/")
-                            .client(get())
-                            .addConverterFactory(GsonConverterFactory.create(get()))
+                            .addConverterFactory(GsonConverterFactory.create())
                             .build()
                     }
 
-                    // Repositories
-                    singleOf(::PrayerRepository)
-                    singleOf(::WeatherRepository)
+                    single { PrayerRepository(get()) }
+                    single { WeatherRepository(get()) }
 
-                    // ViewModels
-                    viewModelOf(::HomeViewModel)
-                    viewModelOf(::DhikrViewModel)
+                    viewModel { HomeViewModel(get(), get(), get()) }
+                    viewModel { DhikrViewModel(get()) }
                 }
             )
         }
