f73f4d88a9d1490afd423cfcf1c2adc12c84e049
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-23 17:20:39 +0300

    Update AppModule.kt

--- app/src/main/java/com/alaa/di/AppModule.kt
+++ app/src/main/java/com/alaa/di/AppModule.kt
@@ -1,14 +1,2 @@
 package com.alaa.di
-
-import com.alaa.domain.usecase.PrayerSchedulingUseCase
-import com.alaa.presentation.screen.dhikr.DhikrViewModel
-import com.alaa.presentation.screen.home.HomeViewModel
-import org.koin.android.ext.koin.androidApplication
-import org.koin.androidx.viewmodel.dsl.viewModel
-import org.koin.dsl.module
-
-val appModule = module {
-    single { PrayerSchedulingUseCase(androidApplication()) }
-    viewModel { DhikrViewModel(androidApplication()) }
-    viewModel { HomeViewModel(androidApplication()) }
-}
+// All DI is in App.kt
