0dcfe7ababf3a4e1671af2059d68c9fda98730b8
alaaaalaa8282-cell <alaaaalaa82.82@gmail.com>
2026-05-26 11:30:18 +0300

    Update Screen.kt

--- app/src/main/java/com/alaa/navigation/Screen.kt
+++ app/src/main/java/com/alaa/navigation/Screen.kt
@@ -5,7 +5,7 @@ sealed class Screen(val route: String) {
     data object PrayerTimes  : Screen("prayer_times")
     data object Dhikr        : Screen("dhikr")
     data object Mesbaha      : Screen("mesbaha")
-    data object Challenges   : Screen("challenges")
+    data object Mushaf      : Screen("mushaf")
     data object Quran        : Screen("quran")
     data object Radio        : Screen("radio")
     data object Adhkar.      : Screen("adhkar")
